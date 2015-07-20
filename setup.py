7fbe00ed5aa0efa0a3b6f60a954f59203b412d84
Jonathan A. Gross <jarthurgross@gmail.com>
2015-07-20 15:21:28 -0600

    Configured setup to grab dependencies

--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
 from setuptools import setup
 
+# Workaround from
+# https://github.com/numpy/numpy/issues/2434#issuecomment-65252402
+setup_requires=['numpy']
+
 requires = [
         'numpy',
         'scipy',
-        'qinfer',
-        'bloch_distribution',
-        'qubit_dst',
+        ]
+
+dependency_links = [
+        'https://github.com/csferrie/python-qinfer.git',
+        'https://github.com/jarthurgross/bloch_distribution.git',
+        'https://github.com/jarthurgross/qubit_dst.git',
         ]
 
 setup(name='seq_mon_car',
       version='0.0',
       py_modules=['model', 'dists', 'simulation'],
-      install_requires=requires
+      setup_requires=setup_requires,
+      install_requires=requires,
+      dependency_links=dependency_links,
+      packages=['seq_mon_car'],
+      package_dir={'seq_mon_car': 'src/seq_mon_car'},
      )
